3876e61e0558b941bd651312fe612f88311b4f4e
Michael Knoch <miknoch@htwg-konstanz.de>
2015-10-20 14:43:07 +0200

    implement getopt with new config

--- V1/start.py
+++ V1/start.py
@@ -12,22 +12,44 @@
 #============================================================================
 
 import os
+import sys
+import getopt
 
-print('Script started...')
+config = False
 
-# download and extract kernel
+def init():
+	print('Script started...')
+	# download and extract kernel
 
-if not os.path.isfile('linux-4.2.3.tar.xz'):
-	os.system('wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.2.3.tar.xz')
-	print('Download Success')
-else:
-	print('file already exists')
+	if not os.path.isfile('linux-4.2.3.tar.xz'):
+		os.system('wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.2.3.tar.xz')
+		print('Download Success')
+	else:
+		print('file already exists')
 
-os.system('tar xfv linux-4.2.3.tar.xz')
-print('extract Success')
+	os.system('tar xfv linux-4.2.3.tar.xz')
+	print('extract Success')
 
-# run allnoconfig & menuconfig when .config is not present
-if not os.path.isfile('linux-4.2.3/.config'):
-	os.system('cd linux-4.2.3 && make ARCH=i386 allnoconfig')
-	os.system('cd linux-4.2.3 && make ARCH=i386 menuconfig')
+	# run allnoconfig & menuconfig when .config is not present
+	if config:
+		os.system('cd linux-4.2.3 && make ARCH=i386 allnoconfig')
+		os.system('cd linux-4.2.3 && make ARCH=i386 menuconfig')
 
+
+def main(argv):
+	try:
+		opts, args = getopt.getopt(argv,"c", ["config"])
+   	except getopt.GetoptError:
+      		print 'argument parse error'
+      		sys.exit(2)
+   	for opt, arg in opts:
+      		if opt in ("-c", "--config"):
+         		config = True
+			print 'new config'
+	
+	print 'start default'
+	init()
+	sys.exit(0)			
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
