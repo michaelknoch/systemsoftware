3680941fb1036e3807f296be39fa84239e9779a5
Michael Knoch <miknoch@htwg-konstanz.de>
2015-10-20 12:33:22 +0200

    start script init

---
+++ V1/start.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding: utf8
+
+#============================================================================
+#   Assignment: start.py SYSO
+#
+#       Author:  Timo Weiß, Michael Knoch
+#
+#   Instructor:  Michael Maechtel, Michael Offergeld
+#         Date:  20.10.2015
+#
+#============================================================================
+
+print('Script started...')
