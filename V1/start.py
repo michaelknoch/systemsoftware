ef466fcc8b7abb94f22728d4aedf1a7ed3b84e5f
Michael Knoch <miknoch@htwg-konstanz.de>
2015-10-20 12:47:29 +0200

    implement download and extract

--- V1/start.py
+++ V1/start.py
@@ -11,4 +11,17 @@
 #
 #============================================================================
 
+import os
+
 print('Script started...')
+
+# download and extract kernel
+
+if not os.path.isfile('linux-4.2.3.tar.xz'):
+	os.system('wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.2.3.tar.xz')
+	print('Download Success')
+else:
+	print('file already exists')
+
+os.system('tar xfv linux-4.2.3.tar.xz')
+print('extract Success')
