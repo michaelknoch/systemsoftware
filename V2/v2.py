62ba2f15a4bea6e6dcd7b6d80e1be58410c883a2
Michael Knoch <miknoch@htwg-konstanz.de>
2015-11-03 12:49:22 +0100

    modify qemu start command

--- V2/v2.py
+++ V2/v2.py
@@ -79,7 +79,7 @@ def copyInitFs():
 	os.system('cp initfs linux-4.2.3/')	
 
 def runQemu():
-	os.system('qemu-system-i386 -kernel linux-4.2.3/arch/x86/boot/bzImage -nographic -append "console=ttyS0" -initrd ./initramfs_data.cpio.gz')
+	os.system('qemu-system-arm -M vexpress-a9 -kernel linux-4.2.3/arch/arm/boot/bzImage -nographic -append "console=ttyS0" -initrd ./initramfs_data.cpio.gz')
 
 def copyConfigFile(into=False):
 	# if into True copy config file from current directory into linux-4.2.3
