efe70ba2a87e0af58bade9c8528bc6a13e4fdfaa
Timo Weiss <tiweiss@htwg-konstanz.de>
2015-11-10 17:55:50 +0100

    various changes for dhcp

--- V2/init.sh
+++ V2/init.sh
@@ -1,4 +1,5 @@
 #!/bin/busybox sh
+
 /bin/busybox --install -s /bin/
 
 
@@ -17,14 +18,30 @@ mount -t tmpfs none /tmp
 mount -t sysfs none /sys
 mount -t proc proc /proc
 
+mount -t devpts devpts /dev/pts
 mdev -s
 
 
+/bin/chown root.root /bin/busybox
+/bin/chmod 4755 /bin/busybox
+
 # Set loglevel to "Emergency messages, system is about to crash or is unstable"
 echo 0 > /proc/sys/kernel/printk
+
+ls /etc
+ls /etc/udhcp
+ls /bin
+ls /usr/share/udhcpc
+
+udhcpc -i eth0 -s /etc/udhcp/sample.script
 
 #feuer frei
 ./bin/sysinfo
 
 ## shell feuern
-setsid sh
+echo 'asd'
+ifconfig
+echo 'asd2'
+#setsid cttyhack sh
+
+exec sh
