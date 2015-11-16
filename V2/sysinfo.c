a514bb9482680c8060576f12bba263a9be044ce6
Michael Knoch <miknoch@htwg-konstanz.de>
2015-11-16 12:47:06 +0100

    improve sysinfo, remove warnings

--- V2/sysinfo.c
+++ V2/sysinfo.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <sys/sysinfo.h>
-// http://man7.org/linux/man-pages/man2/getpagesize.2.html
+/* http://man7.org/linux/man-pages/man2/getpagesize.2.html */
 #include <unistd.h>
 #include <sys/utsname.h>
                                                  
 int main(void)
 {
+
+	int gethostname(char *name, size_t len);
+	int getpagesize(void);
+
 	struct sysinfo sys;
 	struct utsname uts;
+
+	char hostname[1024];
+	hostname[1023] = '\0';
 	
 	if (sysinfo(&sys) != 0) {
 		fprintf( stderr, "Hoppla!" );
@@ -19,9 +26,7 @@ int main(void)
 		return 1;
 	}
 
-	//http://stackoverflow.com/questions/504810/how-do-i-find-the-current-machines-full-hostname-in-c-hostname-and-domain-infos
-	char hostname[1024];
-	hostname[1023] = '\0';
+	/* http://stackoverflow.com/questions/504810/how-do-i-find-the-current-machines-full-hostname-in-c-hostname-and-domain-infos */
 	gethostname(hostname, 1023);
 
 	printf("Hello User world:\n");
@@ -34,7 +39,7 @@ int main(void)
 	printf("Total RAM: %lu MB\n", sys.totalram / 1024 / 1024);
 	printf("Free RAM: %lu MB\n", sys.freeram / 1024 / 1024);
 	printf("Process Count: %hu\n", sys.procs);
-	printf("Page size: %ldB\n", getpagesize());
+	printf("Page size: %dB\n", getpagesize());
 
 	return 0;
 }
