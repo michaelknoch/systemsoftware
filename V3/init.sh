cb47b8cf26b0be0f36a9239cd729950804a529b8
Timo Weiss <tiweiss@htwg-konstanz.de>
2015-11-23 13:08:57 +0100

    cp init from v3

---
+++ V3/init.sh
@@ -0,0 +1,49 @@
+#!/bin/busybox sh
+
+/bin/busybox --install -s /bin/
+
+
+echo '!!!!!!!'
+echo 'wuhoooo'
+echo 'wuhoooo'
+echo 'wuhoooo'
+echo 'wuhoooo'
+echo 'wuhoooo'
+echo '!!!!!!!'
+
+
+## mount (-t = target)
+## type device dir
+mount -t tmpfs none /tmp
+mount -t sysfs none /sys
+mount -t proc proc /proc
+
+mount -t devpts devpts /dev/pts
+
+
+/bin/chown 0.0 /bin/busybox
+/bin/chmod 4755 /bin/busybox
+
+mdev -s
+
+# Set loglevel to "Emergency messages, system is about to crash or is unstable"
+echo 0 > /proc/sys/kernel/printk
+
+#chmod 777 /etc/udhcp/simple.script
+#ls -la /etc/udhcp/simple.script
+#whoami
+udhcpc -i eth0 -s /etc/udhcp/simple.script
+
+#feuer frei
+./bin/sysinfo
+
+## shell feuern
+#echo 'asd'
+#ifconfig
+#echo 'asd2'
+
+telnetd 192.168.26.48 -l /bin/sh
+
+setsid cttyhack /bin/busybox sh
+
+
