00ae1b817bec7c237302920b9805ebcbceee5c4a
Timo Weiss <tiweiss@htwg-konstanz.de>
2015-12-12 17:55:24 +0100

    add openclose

--- V4/v4cp.sh
+++ V4/v4cp.sh
@@ -3,8 +3,10 @@
 # TODO: create tar archive
 
 cd modul-src/
-tar -cvzf ../tars/template-1.0.tar.gz template/
+tar -cvzf ../tars/template-1.0.tar.gz template
+tar -cvzf ../tars/openclose-1.0.tar.gz openclose
 cd -
+
 
 cd tars
 mv ./* ../../V3/buildroot/dl
