4d1d29086191de00faf62df76bc31c788706951b
Michael Knoch <miknoch@htwg-konstanz.de>
2015-12-13 17:16:20 +0100

    alloc minors_count

--- V4/modul-src/openclose/openclose.c
+++ V4/modul-src/openclose/openclose.c
@@ -9,6 +9,7 @@
 #include <linux/device.h>
 
 #define DRIVER_NAME "openclose"
+#define MINORS_COUNT = 2
 #define PROCESS_COUNT 1
 
 
@@ -84,7 +85,7 @@ static int __init ModInit(void)
 {
 	int major;
 
-	if( alloc_chrdev_region( &device_number, 0, 1, DRIVER_NAME ) < 0) {
+	if( alloc_chrdev_region( &device_number, 0, MINORS_COUNT, DRIVER_NAME ) < 0) {
 		printk("Devicenumber 0x%x not available ...\n", device_number );
 		return -1;
 	}
@@ -100,7 +101,7 @@ static int __init ModInit(void)
 	driver_object->owner = THIS_MODULE;
 
 
-	if( cdev_add( driver_object, device_number, 1 )) {
+	if( cdev_add( driver_object, device_number, MINORS_COUNT )) {
 		printk("cdev_add failed ...\n");
 		goto free_cdev;
 	} else {
