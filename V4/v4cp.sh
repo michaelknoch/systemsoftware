06b43d273ca971fff94ba4d1617ef88f214550cb
Timo Weiss <tiweiss@htwg-konstanz.de>
2015-12-14 17:45:42 +0100

    add mynull and myzero

--- V4/v4cp.sh
+++ V4/v4cp.sh
@@ -6,6 +6,8 @@ cd modul-src/
 tar -cvzf ../tars/template-1.0.tar.gz template
 tar -cvzf ../tars/openclose-1.0.tar.gz openclose
 tar -cvzf ../tars/access-1.0.tar.gz access
+tar -cvzf ../tars/mynull-1.0.tar.gz mynull
+tar -cvzf ../tars/myzero-1.0.tar.gz myzero
 cd -
 
 
