48c07289e8ebe1afe99f4baec16d62fd46cbe5eb
Timo Weiss <tiweiss@htwg-konstanz.de>
2016-01-09 17:36:14 +0100

    add open, release, read and write

--- V5/modul-src/buf/buf.c
+++ V5/modul-src/buf/buf.c
@@ -16,7 +16,10 @@ static struct cdev *driver_object;
 static dev_t device_number;
 struct class *template_class;
 
-
+static int driver_open(struct inode *geraetedatei, struct file *instanz); 
+static int driver_release(struct inode *geraetedatei, struct file *instanz); 
+static ssize_t driver_read(struct file *instanz, char *user, size_t count, loff_t *offset);
+static ssize_t driver_write(struct file *instanz, const char __user *user, size_t count, loff_t *offs);
 
 static int __init ModInit(void)
 {
