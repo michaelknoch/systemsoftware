c0723a331f8fbb1f203389896b85b5a3c28dadd9
Timo Weiß <info@timo-weiss.com>
2016-01-10 13:24:57 +0100

    robot work

--- V5/modul-package/timer/buf.mk
+++ V5/modul-package/timer/buf.mk
@@ -1,28 +1,28 @@
-##### BUF
+##### TIMER
 
-BUF_VERSION = 1.0
-BUF_SITE_METHOD = file
-BUF_NAME = buf
-BUF_SITE = $(BUF_NAME)-$(BUF_VERSION).tar.gz
+TIMER_VERSION = 1.0
+TIMER_SITE_METHOD = file
+TIMER_NAME = timer
+TIMER_SITE = $(TIMER_NAME)-$(TIMER_VERSION).tar.gz
 
 
-BUF_DEPENDENCIES = linux
+TIMER_DEPENDENCIES = linux
 
-define BUF_BUILD_CMDS
+define TIMER_BUILD_CMDS
 	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules
 endef
 
-define BUF_INSTALL_TARGET_CMDS
+define TIMER_INSTALL_TARGET_CMDS
 	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install
-	$(INSTALL) -m 0755 -D $(@D)/test_$(BUF_NAME).sh $(TARGET_DIR)/usr/bin/test_$(BUF_NAME).sh
+	$(INSTALL) -m 0755 -D $(@D)/test_$(TIMER_NAME).sh $(TARGET_DIR)/usr/bin/test_$(TIMER_NAME).sh
 endef
 
-define BUF_CLEAN_CMDS
+define TIMER_CLEAN_CMDS
 	$(MAKE) -C $(@D) clean
 endef
 
-define BUF_UNINSTALL_TARGET_CMS
-	rm $(TARGET_DIR)/usr/bin/$(BUF_NAME)
+define TIMER_UNINSTALL_TARGET_CMS
+	rm $(TARGET_DIR)/usr/bin/$(TIMER_NAME)
 endef
 
 $(eval $(generic-package))
