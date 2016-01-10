b12f9427d4f0a83005eb106687da4596cd1a8a55
Timo Weiß <info@timo-weiss.com>
2016-01-10 13:12:56 +0100

    rename to kthread

--- V5/modul-package/kthread/openclose.mk
+++ V5/modul-package/kthread/openclose.mk
@@ -2,19 +2,19 @@
 
 OPENCLOSE_VERSION = 1.0
 OPENCLOSE_SITE_METHOD = file
-OPENCLOSE_NAME = openclose
+OPENCLOSE_NAME = kthread
 OPENCLOSE_SITE = $(OPENCLOSE_NAME)-$(OPENCLOSE_VERSION).tar.gz
 
 
 OPENCLOSE_DEPENDENCIES = linux
 
 define OPENCLOSE_BUILD_CMDS
-	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules 
+	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules
 endef
 
 define OPENCLOSE_INSTALL_TARGET_CMDS
 	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install
-	$(INSTALL) -m 0755 -D $(@D)/test_openclose.sh $(TARGET_DIR)/usr/bin/test_openclose.sh
+	$(INSTALL) -m 0755 -D $(@D)/test_kthread.sh $(TARGET_DIR)/usr/bin/test_kthread.sh
 endef
 
 define OPENCLOSE_CLEAN_CMDS
@@ -22,7 +22,7 @@ define OPENCLOSE_CLEAN_CMDS
 endef
 
 define OPENCLOSE_UNINSTALL_TARGET_CMS
-	rm $(TARGET_DIR)/usr/bin/openclose
+	rm $(TARGET_DIR)/usr/bin/kthread
 endef
 
 $(eval $(generic-package))
