d97a544046779b6621d3872a144bd7fe75ad54d3
Timo Weiß <info@timo-weiss.com>
2016-01-10 13:15:35 +0100

    rename

--- V5/modul-package/lock/buf.mk
+++ V5/modul-package/lock/buf.mk
@@ -1,28 +1,28 @@
-##### BUF
+##### LOCK
 
-BUF_VERSION = 1.0
-BUF_SITE_METHOD = file
-BUF_NAME = buf
-BUF_SITE = $(BUF_NAME)-$(BUF_VERSION).tar.gz
+LOCK_VERSION = 1.0
+LOCK_SITE_METHOD = file
+LOCK_NAME = buf
+LOCK_SITE = $(LOCK_NAME)-$(LOCK_VERSION).tar.gz
 
 
-BUF_DEPENDENCIES = linux
+LOCK_DEPENDENCIES = linux
 
-define BUF_BUILD_CMDS
+define LOCK_BUILD_CMDS
 	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules
 endef
 
-define BUF_INSTALL_TARGET_CMDS
+define LOCK_INSTALL_TARGET_CMDS
 	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install
-	$(INSTALL) -m 0755 -D $(@D)/test_$(BUF_NAME).sh $(TARGET_DIR)/usr/bin/test_$(BUF_NAME).sh
+	$(INSTALL) -m 0755 -D $(@D)/test_$(LOCK_NAME).sh $(TARGET_DIR)/usr/bin/test_$(LOCK_NAME).sh
 endef
 
-define BUF_CLEAN_CMDS
+define LOCK_CLEAN_CMDS
 	$(MAKE) -C $(@D) clean
 endef
 
-define BUF_UNINSTALL_TARGET_CMS
-	rm $(TARGET_DIR)/usr/bin/$(BUF_NAME)
+define LOCK_UNINSTALL_TARGET_CMS
+	rm $(TARGET_DIR)/usr/bin/$(LOCK_NAME)
 endef
 
 $(eval $(generic-package))
