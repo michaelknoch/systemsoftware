383d685bdaeb930a5206994e15dfcecee2bad107
Michael Knoch <miknoch@htwg-konstanz.de>
2016-02-03 17:32:18 +0100

    improve

--- preparation/wq/wq.c
+++ preparation/wq/wq.c
@@ -4,13 +4,15 @@
 
 static struct workqueue_struct *wq;
 static void work_to_do(struct work_struct*);
-static DECLARE_WORK( work_obj, work_to_do);
+
+// takes a function work_to_do and declares queable work_obj
+static DECLARE_WORK(work_obj, work_to_do);
+// declares completion object
 static DECLARE_COMPLETION(on_exit);
 
 static atomic_t stop_timer = ATOMIC_INIT(0);
 
-static void work_to_do(struct work_struct *work)
-{
+static void work_to_do(struct work_struct *work) {
 	msleep(2000);
 
 	if (atomic_read(&stop_timer)) {
@@ -25,28 +27,29 @@ static void work_to_do(struct work_struct *work)
 	}
 }
 
-static int __init ModInit(void)
-{
+static int __init ModInit(void) {
 	// ..
-
-	wq = create_workqueue("DrvrSmpl");
+	// define wq with threadname
+	wq = create_workqueue("Threadname");
+	
+	// add work_obj to work que
 	if(queue_work(wq, &work_obj)) {
-		printk( "queue_work successful ...\n");
+		printk("queue_work successful ...\n");
 	} else {
-		printk( "queue_work not successful ...\n");
+		printk("queue_work not successful ...\n");
 	}
 	return 0;
+}
 
-	static void __exit ModExit(void)
-	{
-		atomic_set(&stop_timer, 1);
-		wait_for_completion(&on_exit);
-		if( wq ) {
-			destroy_workqueue( wq );
-			pr_debug("workqueue destroyed\n");
-		}
+static void __exit ModExit(void) {
+	atomic_set(&stop_timer, 1);
+	wait_for_completion(&on_exit);
+	if(wq) {
+		destroy_workqueue(wq);
+		pr_debug("workqueue destroyed\n");
+	}
 
 	// ..
-	}
+}
 
 // ..
