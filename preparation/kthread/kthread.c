2b69d47447d679c6415eccc269e2eb07d47b2e3e
Michael Knoch <info@michaelknoch.de>
2016-02-04 11:27:47 +0100

    reformat

--- preparation/kthread/kthread.c
+++ preparation/kthread/kthread.c
@@ -10,32 +10,32 @@ static wait_queue_head_t wq;
 static DECLARE_COMPLETION(on_exit);
 
 static int thread_code( void *data ) {
-    unsigned long timeout;
+	unsigned long timeout;
 
-    allow_signal(SIGTERM); 
-    while(!signal_pending(current)) {
-        timeout= 2 * HZ;
-        timeout=wait_event_interruptible_timeout(
-            wq, (timeout==0), timeout);
-        printk("thread_code: woke up after 2 secs ...\n");
-        if(timeout==-ERESTARTSYS) {
-            printk("got signal, break\n");
-            break;
-        }
-    }
-    thread_id = 0;
-    complete_and_exit(&on_exit, 0);
+	allow_signal(SIGTERM); 
+	while(!signal_pending(current)) {
+		timeout= 2 * HZ;
+		timeout=wait_event_interruptible_timeout(
+			wq, (timeout==0), timeout);
+		printk("thread_code: woke up after 2 secs ...\n");
+		if(timeout==-ERESTARTSYS) {
+			printk("got signal, break\n");
+			break;
+		}
+	}
+	thread_id = 0;
+	complete_and_exit(&on_exit, 0);
 }
 
 static int __init ModInit(void) {
 	// ...
 
 	init_waitqueue_head(&wq);
-    thread_id=kthread_create(thread_code, NULL, "MyKThread");
-    if(thread_id==0) {
-        return -EIO;
-    }
-    wake_up_process(thread_id);
+	thread_id=kthread_create(thread_code, NULL, "MyKThread");
+	if(thread_id==0) {
+		return -EIO;
+	}
+	wake_up_process(thread_id);
 	return 0;
 }
 
