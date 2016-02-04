7a19e2bb3e5b456cbfc9e2436250e8effa5e5bc9
Michael Knoch <info@michaelknoch.de>
2016-02-04 19:49:46 +0100

    Update read_write.c

--- preparation/read_write.c
+++ preparation/read_write.c
@@ -7,9 +7,8 @@ static ssize_t driver_write(struct file *instanz, const char *user, size_t count
 	char buffer[count];
 	unsigned long notcopied;
 	size_t to_copy;
-
-	to_copy =  count;
 	
+	to_copy = count;
 	// unsigned long copy_from_user (void * to, const void __user * from, unsigned long n);
 	notcopied = copy_from_user(buffer, user, to_copy);
 	return to_copy - notcopied;
@@ -20,7 +19,7 @@ static ssize_t driver_read(struct file *instanz, char *user, size_t count, loff_
 	unsigned long notcopied;
 	size_t to_copy;
 
-	to_copy =  min(strlen(minorone), count);
+	to_copy =  min(strlen(helloworld) + 1, count);
 	
 	// unsigned long copy_to_user(void __user * to, const void * from, unsigned long n);
 	notcopied = copy_to_user(user, helloworld, to_copy);
