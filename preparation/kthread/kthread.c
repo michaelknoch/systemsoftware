109abb2cdcf23e07710d57da467c303fa4f3499d
Michael Knoch <info@michaelknoch.de>
2016-02-04 11:57:15 +0100

    improve

--- preparation/kthread/kthread.c
+++ preparation/kthread/kthread.c
@@ -2,6 +2,7 @@
 #include <linux/completion.h>
 #include <linux/sched.h>
 #include <linux/kthread.h>
+#include <asm/signal.h>
 
 // ...
 
@@ -12,18 +13,23 @@ static DECLARE_COMPLETION(on_exit);
 static int thread_code( void *data ) {
 	unsigned long timeout;
 
+	// erlaubt signal SIGTERM für aktuellen thread
 	allow_signal(SIGTERM); 
-	while(!signal_pending(current)) {
-		timeout= 2 * HZ;
-		timeout=wait_event_interruptible_timeout(
-			wq, (timeout==0), timeout);
+	for (int i = 0; i < 100; i++) {
+		timeout = 2 * HZ;
+		/* 	versetzt aktuellen thread in zustand warten (interruptible), wenn condition (timeout == 0) false
+			wird aufgeweckt wenn timeout abgelaufen ist oder on signal
+		*/
+		timeout = wait_event_interruptible_timeout(wq, (timeout == 0), timeout);
 		printk("thread_code: woke up after 2 secs ...\n");
-		if(timeout==-ERESTARTSYS) {
+		// wird von signal geweckt
+		if(timeout == -ERESTARTSYS) {
 			printk("got signal, break\n");
 			break;
 		}
 	}
 	thread_id = 0;
+	// complete and exit with returnvalue 0
 	complete_and_exit(&on_exit, 0);
 }
 
@@ -31,8 +37,8 @@ static int __init ModInit(void) {
 	// ...
 
 	init_waitqueue_head(&wq);
-	thread_id=kthread_create(thread_code, NULL, "MyKThread");
-	if(thread_id==0) {
+	thread_id = kthread_create(thread_code, NULL, "MyKThread");
+	if(thread_id == 0) {
 		return -EIO;
 	}
 	wake_up_process(thread_id);
@@ -43,9 +49,8 @@ static void __exit ModExit(void) {
 	if(thread_id){
 		kill_pid(task_pid(thread_id), SIGTERM, 1);
 	}
-	
 	wait_for_completion(&on_exit);
-
+	
 	// ...	
 }
 
